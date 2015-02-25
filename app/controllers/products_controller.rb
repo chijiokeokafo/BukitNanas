944642e99a197d5c765282d13affa0202f4ccacf
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-02-25 16:30:21 -0500

    Adding XHR object to index.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
   def index
   	@products = if params[:search]
-     Product.where("ILIKE", "%#{params[:search]}%")
+      Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
     else 
       Product.all
+    end
+
+    if request.xhr?
+      render @products
     end
   end
 
